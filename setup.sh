b36b9680aac2ee6dda509e206fcbde6acd4dbe2d
Devin Jhu <60712826+devin-jhu@users.noreply.github.com>
2022-09-16 19:00:53 -0400

    2.0

--- setup.sh
+++ setup.sh
@@ -22,4 +22,37 @@ echo ""
 cat ~/.ssh/id_ed25519.pub
 echo "paste that key ^ into github ssh then press ENTER when you sre done"
 read any_key
-echo
+echo ""
+echo "type yes and press any key DO NOT OVERTHINK IT"
+read any_key
+ssh -T git@github.comands
+
+#sum extra stuff to maka looka niceh
+echo "Load programs"
+sudo apt install tree
+#neovim time
+sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake get++ pkg-config unzip curl doxygen
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+#back it up
+cd..
+
+#tha lovely .bashrc makes an entrance
+echo "load .bashrc file"
+> ~/.bashrc
+cp ~/dotfiles/.bashrc ~/.bashrc
+
+#typescript
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+#typescript bashrc 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash completion
+#back to reality 
+nvm install node --lst
+nvm install-latest-npm
+#JAVA TIME
+echo "load java"
+sudo apt install default-jdk -y
+
