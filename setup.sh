1de63107b0bbf505502a41c9adc79cc72b573cd9
Devin Jhu <60712826+devin-jhu@users.noreply.github.com>
2022-09-28 09:26:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #
 #setup comands cuz im lazy :)
 #
-echo Update and upgrade system
+echo "Update and upgrade system" 
 sudo apt-get update
 sudo apt-get full-upgrade -y
 
@@ -27,45 +27,57 @@ echo "type yes and press any key DO NOT OVERTHINK IT"
 read any_key
 ssh -T git@github.comands
 
-#sum extra stuff to maka looka niceh
-echo "Load programs"
-sudo apt install tree
-#neovim time
-sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake get++ pkg-config unzip curl doxygen
+# NeoVim
+sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-#back it up
-cd..
+cd ..
 
-#tha lovely .bashrc makes an entrance
-echo "load .bashrc file"
-> ~/.bashrc
+echo Load .bashrc file
 cp ~/dotfiles/.bashrc ~/.bashrc
 
-#typescript
+echo load nvim options
+cp -r ~/dotfiles/config ~/.config
+
+# Typel script
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-#typescript bashrc 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads nvm bash completion
-#back to reality 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" #loads vim
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" #loads
 nvm install node --lst
 nvm install-latest-npm
-#JAVA TIME
-echo "load java"
+
+# load java software
+echo load java
 sudo apt install default-jdk -y
 
-#remove dotfiles directory
+# script
+echo load shell script file
+mkdir ~/scripts
+
+# java linter
+wget https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.3.3/checkstyle-10.3.3-all.jar
+cp ./checkstyle-10.3.3-all.jar ~/scripts/checkstyle.jar
+wget https://raw.githubusercontent.com/Mr-Coxall/dot_files/main/mr-coxall_checks.xml
+cp ./mr-coxall_checks.xml ~/scripts/
+rm checkstyle-10.3.3-all.jar
+rm mr-coxall_checks.xml
+
+# rid of dotfiles
 sudo rm -R ~/dotfiles
 
-#folders
 mkdir ~/ICS4U
-mkdir ~/ICS4U/Unit1
-cd ..
-mkdir ~/ICS4U/Assignments
-#reboot
-echo "---"
-echo "rebooting now..."
-echo "---"
-sudo reboot now 
+mkdir -p ~/ICS4U/Unit1/Unit1-02
+mkdir ~/ICS4U/Unit1/Unit1-03
+mkdir ~/ICS4U/Unit1/Unit1-04
+mkdir ~/ICS4U/Unit1/Unit1-05
+mkdir ~/ICS4U/Unit1/Unit1-06
+mkdir ~/ICS4U/Unit1/Unit1-07
+# ... 
+
+# reboot
+echo ---
+echo rebooting now ...
+echo ---
+sudo reboot now
